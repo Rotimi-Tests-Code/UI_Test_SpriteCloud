a74268af0ebdfea3dcaa4ab50cfe74f49f73357e
Oluwarotimi Samuel Adegoju <oluwarotimisamuel.adegoju@griffithuni.edu.au>
2025-03-19 17:50:51 +0100

    Updated Cypress to latest version for CI compatability

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,7 @@
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      //AI Implementation
-      on('before:browser:launch', (browser = {}, launchOptions) => {
-        if (browser.name === 'chrome') {
-          launchOptions.args.push('--disable-feautures=AutofillServerCommunicaiton,PasswordManagerEnforcement');
-        }
-        return launchOptions;
-      });
+      
       return config;
       
       // implement node event listeners here
