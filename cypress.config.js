c402fad4383d94104e4236820e49091d6ee96110
Oluwarotimi Samuel Adegoju <oluwarotimisamuel.adegoju@griffithuni.edu.au>
2025-03-19 17:56:04 +0100

    Fix: added missing defineConfig import to Cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,10 @@
+const { defineConfig } = require('cypress');
+
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      
-      return config;
-      
       // implement node event listeners here
+      return config;
     },
-  
   },
 });
