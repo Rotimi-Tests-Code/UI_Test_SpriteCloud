6b097e7d9e4d88f38fc8097085c429ad733bb284
Oluwarotimi Samuel Adegoju <oluwarotimisamuel.adegoju@griffithuni.edu.au>
2025-03-20 23:23:13 +0100

    Refined API_Tests.cy.js to change value from 3100 to 3300

--- cypress/e2e/API_Tests/API_Tests7.cy.js
+++ cypress/e2e/API_Tests/API_Tests7.cy.js
@@ -1,19 +1,30 @@
 describe('Execute a parametrized delay request max 3 seconds. ', () => {
     it('excecutes a delayed API response', () => {
-    /* AI helped me understand the consept of Date.now.
-     I implemented this porition of the code myself  */
+  
+  /* AI helped me with the understanding of let start and the implementation of 
+  cy.window */
+    let start;
+
+   cy.window().then((win ) => {
+    start = win.performance.now();
+   })
+   
+        
      /* Attached the date.now to the start */
-        const start = Date.now();
         cy.request('GET', 'https://reqres.in/api/users?delay=3',
         ).then ((response) => {
             /* Attach the date now to an end constant */
-            const end = Date.now();
+            cy.window().then((win) => { 
+            const end = win.performance.now();
             const duration = end - start;
             /* Validating that the response is a 200 */
             expect(response.status).to.equal(200);
             /* AI helped with this implementation of 3100 because I kept using 3000
             Now my expected duration should be below 3000 */
-            expect(duration).to.be.lessThan(3100);
+            expect(parseFloat(duration)).to.be.lessThan(3300);
+
+            cy.log(`Duration: ${parseFloat(duration).toFixed(2)}ms`);
+        })
 
         })
     })
